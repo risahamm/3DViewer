b0fd3c03098592e73b02e5c40dfdecde9d8d5a09
kattiede <kattiede@student.21-school.ru>
2024-10-29 19:59:05 +0300

    CMAKE rabotaet!!!

--- src/Model/main.cc
+++ src/Model/main.cc
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main() {
-  //    string str = "../src/object_files/test21.obj";
-  string str = "../object_files/test21.obj";
+  string str = "/Users/kattiede/CLionProjects/3DViewer/src/object_files/test21.obj";
   s21::Object Obj;
   Obj.Parser(str);
   Obj.PrintVertices();
