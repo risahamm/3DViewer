bf58bd118533b48011c3e6c55ed4a9694af2056f
risahamm <risahamm@student.21-school.ru>
2024-11-30 17:51:35 +0300

    UI ui update

--- src/Model/main_test.cc
+++ src/Model/main_test.cc
@@ -3,11 +3,17 @@
 using namespace std;
 
 int main() {
-  string str = "/Users/risahamm/projects/3DViewer/src/object_files/teapot.obj";
-//  string str = "../../object_files/test_cube.obj";
-  s21::Object Obj;
-  Obj.Parser(str);
+//  string str = "/Users/risahamm/projects/3DViewer/src/object_files/teapot.obj";
+////  string str = "../../object_files/test_cube.obj";
+//  s21::Object Obj;
+//  Obj.Parser(str);
 //  Obj.PrintFacets();
+
+  string vertex_str = "8854/23";
+
+  size_t pos = vertex_str.find('/'); // || vertex_str.find(' ');
+
+  std::cout << "pos = " << pos << endl;
 
   return 0;
 }
--- src/main.cc
+++ src/main.cc
@@ -9,7 +9,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     s21::Object object;
     s21::Controller controller(&object);
-    View w(nullptr, &controller);
-    w.show();
+    View app(nullptr, &controller);
+
+    app.setWindowIcon(QIcon(":/View/icons/app_icon.png"));
+    app.setWindowTitle("3D Viewer");
+
+    app.show();
     return a.exec();
 }
