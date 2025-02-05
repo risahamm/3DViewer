06e11bb4e5dea9406387006534a11f943b2c4b2f
Anna Shusharina <shusharina.ai@yandex.ru>
2025-02-05 20:53:47 +0300

    начала тесты, надо проверить

--- src/main_test.cc
+++ src/main_test.cc
@@ -1,10 +1,34 @@
 #include "Model/object.h"
+#include "Controller/controller.h"
+#include "Model/Transformations/move_object.h"
+#include "Model/Transformations/transformations.h"
+#include "Model/Transformations/zoom_object.h"
 
 using namespace std;
 
 int main() {
 
-  
+  s21::Object obj;
+  s21::Controller controller(&obj);
+
+  controller.OpenFile("../../object_files/cube.obj");
+  controller.MoveXRight(2);
+  std::vector<s21::Point> res = controller.getVertices();
+  s21::Point a(2, 2, 0);
+  s21::Point b(2, 0, 0);
+  s21::Point c(2, 2, 2);
+  s21::Point d(2, 0, 2);
+  s21::Point e(0, 2, 0);
+  s21::Point f(0, 0, 0);
+  s21::Point g(0, 2, 2);
+  s21::Point h(0, 0, 2);
+  std::vector<s21::Point> correct = {a, b, c, d, e, f, g, h};
+
+  if (correct == res) {
+    cout << "true \n";
+  } else {
+    cout << "false \n";
+  }
 
   return 0;
 }
