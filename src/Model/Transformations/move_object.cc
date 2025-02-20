03c18d661dd101a82631c7553f261fee8b338987
risahamm <risahamm@student.21-school.ru>
2025-02-20 20:28:15 +0300

    исправить зум

--- src/Model/Transformations/move_object.cc
+++ src/Model/Transformations/move_object.cc
@@ -1,10 +1,23 @@
 #include "move_object.h"
 
 void s21::MoveObject::Modify(double x, double y, double z) {
-  for (Point &point : object_->getVerticesRef()) {
-    point.x += x;
-    point.y += y;
-    point.z += z;
+//  for (Point &point : object_->getVerticesRef()) {
+//    point.x += x;
+//    point.y += y;
+//    point.z += z;
+//  }
+
+  int size = object_->getVertexCount();
+
+  for (int i = 0; i <= size; i++) {
+    object_->getVerticesRef()[i].x += x;
+    object_->getVerticesRef()[i].y += y;
+    object_->getVerticesRef()[i].z += z;
+
+    object_->getInitialVeritcesRef()[i].x += x;
+    object_->getInitialVeritcesRef()[i].y += y;
+    object_->getInitialVeritcesRef()[i].z += z;
   }
+
   //  object_->setInitialVertices();
 }
