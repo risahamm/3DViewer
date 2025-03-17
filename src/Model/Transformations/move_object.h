1398083ae9248f5c394f02a0103fdb70e0ed535f
a.shusharina <risahamm@student.21-school.ru>
2025-03-17 12:09:40 +0300

    добавила доксиген описание классов ZoomObject и MoveObject

--- src/Model/Transformations/move_object.h
+++ src/Model/Transformations/move_object.h
@@ -5,7 +5,7 @@
 
 namespace s21 {
 
-  /**
+ /**
   * @brief Класс для перемещения объектов.
   * @details Класс наследует от TransformationsBaseClass и
   * предоставляет функциональность для перемещения объектов
@@ -13,36 +13,36 @@ namespace s21 {
   */
   class MoveObject : public TransformationsBaseClass {
 
-  public:
+    public:
 
-  /**
-  * @brief Конструктор класса MoveObject.
-  * @details Инициализирует объект MoveObject, передавая указатель на
-  * объект в базовый класс TransformationsBaseClass.
-  * Это позволяет производить операции перемещения на
-  * заданном объекте.
-  * @param obj Указатель на объект типа s21::Object, который
-  * будет подвергаться преобразованиям.
-  */
-  MoveObject(s21::Object *obj) : TransformationsBaseClass(obj) {};
-
-
-  /**
-  * @brief Деструктор по умолчанию.
-  */
-  ~MoveObject() = default;
+   /**
+    * @brief Конструктор класса MoveObject.
+    * @details Инициализирует объект MoveObject, передавая указатель на
+    * объект в базовый класс TransformationsBaseClass.
+    * Это позволяет производить операции перемещения на
+    * заданном объекте.
+    * @param obj Указатель на объект типа s21::Object, который
+    * будет подвергаться преобразованиям.
+    */
+    MoveObject(s21::Object *obj) : TransformationsBaseClass(obj) {};
 
 
-  /**
-  * @brief Метод для перемещения объекта.
-  * @details Метод переопределяет виртуальный метод базового класса
-  * и выполняет перемещение объекта на заданные значения по
-  * осям x, y и z.
-  * @param x Смещение по оси X.
-  * @param y Смещение по оси Y.
-  * @param z Смещение по оси Z.
-  */
-  void Modify(double x, double y, double z) override;
+   /**
+    * @brief Деструктор по умолчанию.
+    */
+    ~MoveObject() = default;
+
+
+   /**
+    * @brief Метод для перемещения объекта.
+    * @details Метод переопределяет виртуальный метод базового класса
+    * и выполняет перемещение объекта на заданные значения по
+    * осям x, y и z.
+    * @param x Смещение по оси X.
+    * @param y Смещение по оси Y.
+    * @param z Смещение по оси Z.
+    */
+    void Modify(double x, double y, double z) override;
 
   };
 
