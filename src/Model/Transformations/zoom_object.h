dbdace069aafdaee946f4e003071e7c968a863eb
a.shusharina <risahamm@student.21-school.ru>
2025-03-17 11:36:17 +0300

    добавила доксиген описание классов ZoomObject и MoveObject

--- src/Model/Transformations/zoom_object.h
+++ src/Model/Transformations/zoom_object.h
@@ -4,16 +4,48 @@
 #include "transformations.h"
 
 namespace s21 {
+
+  /**
+  * @brief Класс ZoomObject для выполнения операций масштабирования.
+  * @details Класс наследует от TransformationsBaseClass и предоставляет
+  * функциональность для изменения размера объекта. Принимает
+  * указатель на объект типа s21::Object в конструкторе.
+  */
   class ZoomObject : public TransformationsBaseClass {
 
   public:
 
-    ZoomObject(s21::Object *obj) : TransformationsBaseClass(obj) {};
-    ~ZoomObject() = default;
+  /**
+  * @brief Конструктор класса ZoomObject.
+  * @details Инициализирует объект ZoomObject, передавая указатель на
+  * объект в базовый класс TransformationsBaseClass.
+  * Это позволяет производить операции масштабирования на
+  * заданном объекте.
+  * @param obj Указатель на объект типа s21::Object, который
+  * будет подвергаться преобразованиям.
+  */
+  ZoomObject(s21::Object *obj) : TransformationsBaseClass(obj) {};
 
-    void Modify(double x, double y, double z) override;
+
+  /**
+  * @brief Деструктор по умолчанию.
+  */
+  ~ZoomObject() = default;
+
+
+  /**
+  * @brief Метод для изменения размера объекта.
+  * @details Метод масштабирует все вершины объекта, умножая
+  * координаты каждой вершины на соответствующие коэффициенты
+  * масштабирования по осям x, y и z.
+  * @param x Коэффициент масштабирования по оси X.
+  * @param y Коэффициент масштабирования по оси Y.
+  * @param z Коэффициент масштабирования по оси Z.
+  */
+  void Modify(double x, double y, double z) override;
 
   };
+
 } // namespace s21
 
 #endif  // VIEWER_SRC_MODEL_TRANSFORMATIONS_ZOOM_OBJECT_H
